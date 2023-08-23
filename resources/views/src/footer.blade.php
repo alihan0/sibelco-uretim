9c8627663c8d6e2032996dc1778c99b431b8889a
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-23 22:01:16 +0300

    footer created

--- resources/views/src/footer.blade.php
+++ resources/views/src/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                2022 © Apaxy.
+                Copyright © 2023 - Tüm hakları saklıdır.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-right d-none d-sm-block">
-                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
+                    v{{env('VERSION')}}
                 </div>
             </div>
         </div>
