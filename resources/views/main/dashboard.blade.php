1c27645af16aacba4c691fcd615307669969df27
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-23 21:18:44 +0300

    masterpage created

--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -1,1 +1,19 @@
-dashboard
+@extends('master')
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-flex align-items-center justify-content-between">
+            <h4 class="mb-0 font-size-18">Starter Page</h4>
+
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Apaxy</a></li>
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                    <li class="breadcrumb-item active">Starter Page</li>
+                </ol>
+            </div>
+            
+        </div>
+    </div>
+</div>
+@endsection
