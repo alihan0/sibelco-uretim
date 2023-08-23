30aeeb166bf07a8dba4e3e6250316f30989d2a74
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-23 21:42:14 +0300

    topbar complated

--- resources/views/src/topbar.blade.php
+++ resources/views/src/topbar.blade.php
@@ -83,19 +83,15 @@
             <div class="dropdown d-inline-block">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="/static/assets/images/users/avatar-1.jpg"
+                    <img class="rounded-circle header-profile-user" src="/static/assets/images/user.png"
                         alt="Header Avatar">
-                    <span class="d-none d-sm-inline-block ml-1">Shane</span>
+                    <span class="d-none d-sm-inline-block ml-1">{{Auth::user()->name}}</span>
                     <i class="mdi mdi-chevron-down d-none d-sm-inline-block"></i>
                 </button>
                 <div class="dropdown-menu dropdown-menu-right">
                     <!-- item-->
-                    <a class="dropdown-item" href="#"><i class="mdi mdi-face-profile font-size-16 align-middle mr-1"></i> Profile</a>
-                    <a class="dropdown-item" href="#"><i class="mdi mdi-credit-card-outline font-size-16 align-middle mr-1"></i> Billing</a>
-                    <a class="dropdown-item" href="#"><i class="mdi mdi-account-settings font-size-16 align-middle mr-1"></i> Settings</a>
-                    <a class="dropdown-item" href="#"><i class="mdi mdi-lock font-size-16 align-middle mr-1"></i> Lock screen</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#"><i class="mdi mdi-logout font-size-16 align-middle mr-1"></i> Logout</a>
+                    <a class="dropdown-item" href="/auth/logout"><i class="mdi mdi-logout font-size-16 align-middle mr-1"></i> Çıkış Yap</a>
                 </div>
             </div>
 
