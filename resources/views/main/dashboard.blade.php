6905898b2e1ea3e6ce99f879d8a0194969dadb65
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-24 11:41:07 +0300

    dashboard updated

--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -1,15 +1,18 @@
 @extends('master')
+
+@section('title', 'Pano')
+
+
 @section('content')
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-flex align-items-center justify-content-between">
-            <h4 class="mb-0 font-size-18">Starter Page</h4>
+            <h4 class="mb-0 font-size-18">Pano</h4>
 
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
-                    <li class="breadcrumb-item"><a href="javascript: void(0);">Apaxy</a></li>
-                    <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
-                    <li class="breadcrumb-item active">Starter Page</li>
+                    <li class="breadcrumb-item"><a href="/">#</a></li>
+                    <li class="breadcrumb-item active">Pano</li>
                 </ol>
             </div>
             
