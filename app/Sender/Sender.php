a6b269f0b0a8908eba960f35caad9cb8142ebe28
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-25 09:12:52 +0300

    ntification sender creataed

--- app/Sender/Sender.php
+++ app/Sender/Sender.php
@@ -2,15 +2,25 @@
 
 namespace App\Sender;
 
+use App\Models\Notification;
 use Illuminate\Support\Facades\Mail;
 
 class Sender
 {
     public static function email($to, $subject, $data, $template)
-{
-    Mail::send($template, $data, function ($email) use ($to, $subject) {
-        $email->to($to);
-        $email->subject($subject);
-    });
-}
+    {
+        Mail::send($template, $data, function ($email) use ($to, $subject) {
+            $email->to($to);
+            $email->subject($subject);
+        });
+    }
+
+    public static function notification($user, $title, $message){
+        Notification::create([
+            "user" => $user,
+            "title" => $title,
+            "message" => $message,
+            "status" => 1
+        ]);
+    }
 }
