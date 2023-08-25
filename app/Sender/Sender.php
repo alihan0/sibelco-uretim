62e0493b038cee4bda398548c7d255fd724cb391
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-25 10:40:57 +0300

    send sms function created

--- app/Sender/Sender.php
+++ app/Sender/Sender.php
@@ -23,4 +23,39 @@ class Sender
             "status" => 1
         ]);
     }
+
+    public static function sms($to, $message){
+        $number = '90'.$to;
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://smsgw.mutlucell.com/smsgw-ws/sndblkex',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS =>'<?xml version="1.0" encoding="UTF-8"?>
+        <smspack ka="akbiltek" pwd="passAA@2023" org="AKBILSOFT">
+            <mesaj>
+                <metin>'.$message.'</metin>
+                <nums>'.$number.'</nums>
+            </mesaj>
+        </smspack>',
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: text/xml'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        if($response){
+            return response($response);
+        }
+
+    }
 }
