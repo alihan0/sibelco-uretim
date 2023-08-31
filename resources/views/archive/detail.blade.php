a8755b8e229179965a42fb5c96937fe341470e09
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-31 13:10:35 +0300

    form detail header craeted

--- resources/views/archive/detail.blade.php
+++ resources/views/archive/detail.blade.php
@@ -18,19 +18,18 @@
       <div class="card-body">
         <div class="container-fluid d-flex justify-content-between">
           <div class="col-lg-3 ps-0">
-            <a href="#" class="noble-ui-logo d-block mt-3">Noble<span>UI</span></a>                 
-            <p class="mt-1 mb-1"><b>NobleUI Themes</b></p>
-            <p>108,<br> Great Russell St,<br>London, WC1B 3NA.</p>
-            <h5 class="mt-5 mb-2 text-muted">Invoice to :</h5>
-            <p>Joseph E Carr,<br> 102, 102  Crown Street,<br> London, W3 3PR.</p>
+            <a href="#" class="noble-ui-logo d-block mt-3">Sibelco<span>Üretim</span></a>                 
+            <h5 class="mt-3 mb-2 text-muted">Formu Dolduran:</h5>
+            
+            <p>{{$survey->User->name}} <br><span class="text-muted">({{'@'.$survey->User->username}})</span><br> {{$survey->User->email}}<br> {{$survey->User->phone}}</p>
           </div>
           <div class="col-lg-3 pe-0">
-            <h4 class="fw-bold text-uppercase text-end mt-4 mb-2">invoice</h4>
-            <h6 class="text-end mb-5 pb-4"># INV-002308</h6>
-            <p class="text-end mb-1">Balance Due</p>
-            <h4 class="text-end fw-normal">$ 72,420.00</h4>
-            <h6 class="mb-0 mt-3 text-end fw-normal mb-2"><span class="text-muted">Invoice Date :</span> 25rd Jan 2023</h6>
-            <h6 class="text-end fw-normal"><span class="text-muted">Due Date :</span> 12th Jul 2023</h6>
+            <h4 class="fw-bold text-uppercase text-end mt-4 mb-2">Form</h4>
+            <h6 class="text-end mb-5 pb-4"># {{$survey->key}}</h6>
+            
+            
+            <h6 class="mb-0 mt-3 text-end fw-normal mb-2"><span class="text-muted">Oluşturulma Tarihi :</span></h6>
+            <h6 class="text-end fw-normal">  {{$survey->created_at}}</h6>
           </div>
         </div>
         <div class="container-fluid mt-5 d-flex justify-content-center w-100">
@@ -39,10 +38,10 @@
                 <thead>
                   <tr>
                       <th>#</th>
-                      <th>Description</th>
-                      <th class="text-end">Quantity</th>
-                      <th class="text-end">Unit cost</th>
-                      <th class="text-end">Total</th>
+                      <th>Soru</th>
+                      <th class="text-end">Yanıt</th>
+                      <th class="text-end">Fotoğraf</th>
+                      <th class="text-end">Onay</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -78,38 +77,7 @@
               </table>
             </div>
         </div>
-        <div class="container-fluid mt-5 w-100">
-          <div class="row">
-            <div class="col-md-6 ms-auto">
-                <div class="table-responsive">
-                  <table class="table">
-                      <tbody>
-                        <tr>
-                          <td>Sub Total</td>
-                          <td class="text-end">$ 14,900.00</td>
-                        </tr>
-                        <tr>
-                          <td>TAX (12%)</td>
-                          <td class="text-end">$ 1,788.00</td>
-                        </tr>
-                        <tr>
-                          <td class="text-bold-800">Total</td>
-                          <td class="text-bold-800 text-end"> $ 16,688.00</td>
-                        </tr>
-                        <tr>
-                          <td>Payment Made</td>
-                          <td class="text-danger text-end">(-) $ 4,688.00</td>
-                        </tr>
-                        <tr class="bg-light">
-                          <td class="text-bold-800">Balance Due</td>
-                          <td class="text-bold-800 text-end">$ 12,000.00</td>
-                        </tr>
-                      </tbody>
-                  </table>
-                </div>
-            </div>
-          </div>
-        </div>
+        
         <div class="container-fluid w-100">
           <a href="javascript:;" class="btn btn-primary float-end mt-4 ms-2"><i data-feather="send" class="me-3 icon-md"></i>Send Invoice</a>
           <a href="javascript:;" class="btn btn-outline-primary float-end mt-4"><i data-feather="printer" class="me-2 icon-md"></i>Print</a>
